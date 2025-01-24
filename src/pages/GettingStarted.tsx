1515719729aa16dd410f9d87bf677702435fb8fe
omercikan <omercikan36@gmail.com>
2025-01-24 21:32:31 +0300

    Feat:  privacy policy added

--- src/pages/GettingStarted.tsx
+++ src/pages/GettingStarted.tsx
@@ -1,30 +1,63 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ConnectStart from "../assets/images/connect_start.png";
-import PrivacyPolicy from "../components/PrivacyPolicy";
+import PrivacyPolicy from "../components/Privacy/PrivacyPolicy";
+import { useNavigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebaseConfig";
+import Loading from "../components/Loading";
 
 const GettingStarted: React.FC = () => {
   const [isOpenPrivacyModal, setIsOpenPrivacyModal] = useState<boolean>(false);
+  const [user, isLoading] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate("/hesap-olustur");
+    }
+  }, [user, navigate]);
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return (
     <React.Fragment>
       <div className="getting-started-container">
-        <div className="bg-[#0F1828] text-[#F7F7FC] h-screen flex flex-col justify-center items-center text-center select-none">
+        <div className="text-[#F7F7FC] h-screen flex flex-col justify-center items-center text-center">
           <div className="getting-started-wrapper">
             <figure>
-              <img src={ConnectStart} alt="Baslarken" className="w-[325px] max-md:w-[300px] max-sm:w-[250px] mx-auto pointer-events-none mb-10" />
+              <img
+                src={ConnectStart}
+                alt="Baslarken"
+                className="w-[325px] max-md:w-[300px] max-sm:w-[250px] mx-auto pointer-events-none mb-10"
+              />
               <figcaption className="text-2xl max-[500px]:text-xl font-medium">
-                Sevdiklerinizle dünyanın 
+                Sevdiklerinizle dünyanın
                 <br /> her köşesinden anında bağlantı kurun.
               </figcaption>
             </figure>
 
-            <strong className="mt-20 mb-5 block font-normal text-sm cursor-pointer w-max mx-auto" onClick={() => setIsOpenPrivacyModal(true)}>Gizlilik Politikamız</strong>
-            <button className="bg-[#375FFF] px-28 py-3 rounded-[30px] max-[350px]:px-20">Sohbete Başla</button>
+            <strong
+              className="mt-20 mb-5 block font-normal text-sm cursor-pointer w-max mx-auto"
+              onClick={() => setIsOpenPrivacyModal(true)}
+            >
+              Gizlilik Politikamız
+            </strong>
+            <button
+              className="bg-[#375FFF] px-28 py-3 rounded-[30px] max-[350px]:px-20"
+              onClick={() => navigate("/hesap-olustur")}
+            >
+              Sohbete Başla
+            </button>
           </div>
         </div>
       </div>
 
-      <PrivacyPolicy isOpenPrivacyModal={isOpenPrivacyModal} setIsOpenPrivacyModal={setIsOpenPrivacyModal}/>
+      <PrivacyPolicy
+        isOpenPrivacyModal={isOpenPrivacyModal}
+        setIsOpenPrivacyModal={setIsOpenPrivacyModal}
+      />
     </React.Fragment>
   );
 };
