027a92fd6825c62547fbf0daaf07bfc12761a4ca
omercikan <omercikan36@gmail.com>
2025-01-30 19:06:48 +0300

    Updt: Updated to the latest version of 'Tailwind CSS'

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import envCompatible from 'vite-plugin-env-compatible'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   envPrefix: "REACT_APP_",
   plugins: [
     react(),
-    envCompatible()
+    envCompatible(),
+    tailwindcss(),
   ],
 })
