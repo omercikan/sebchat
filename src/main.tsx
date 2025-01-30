6556ad785927960f789e850882125a55ba330ba2
omercikan <omercikan36@gmail.com>
2025-01-30 19:14:08 +0300

    Updated route path component and added main.tsx file general toaster
    -Updated 'hesap-olustur' route path component name
    -Added toaster to global main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,22 @@
 import { createRoot } from "react-dom/client";
 import "./css/index.css";
-import "./scss/main.scss"
+import "./scss/main.scss";
 import { RouterProvider } from "react-router-dom";
-import { MainRouter }from "./routes"
+import { MainRouter } from "./routes";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <RouterProvider router={MainRouter}/>
+    <Toaster
+      position="top-right"
+      reverseOrder={false}
+      toastOptions={{
+        success: { duration: 3000 },
+        error: { duration: 5000 },
+      }}
+    />
+    <RouterProvider router={MainRouter} />
   </Provider>
 );
- 
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import GettingStarted from "./pages/GettingStarted.tsx"
-import PhoneNumber from "./pages/CreateAccount.tsx";
 import Chat from "./pages/Chat.tsx";
+import CreateAccount from "./pages/CreateAccount.tsx";
 
 export const MainRouter = createBrowserRouter([
     { path: "/", element: <GettingStarted /> },
-    { path: "/hesap-olustur", element: <PhoneNumber /> },
+    { path: "/hesap-olustur", element: <CreateAccount /> },
     { path: "/sohbet", element: <Chat /> }
 ])
